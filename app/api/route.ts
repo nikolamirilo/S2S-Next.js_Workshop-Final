77dfe4e4858739472e71a89a262e390fbc26dd32
nikolamirilo <nikolamirilo@gmail.com>
2023-10-02 19:31:41 +0200

    deleted route

--- app/api/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server'
-
-export async function POST() {
-  try {
-    return NextResponse.json("This is API route");
-  } catch (error) {
-    return new NextResponse((error as Error).message);
-  }
-}
